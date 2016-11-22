d5487a16edc29044bf8d200f3cf5adcb13583e9e
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2016-11-22 15:43:19 +0100

    Add project files.

---
+++ HopeAcademySMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HopeAcademySMS.Startup))]
+namespace HopeAcademySMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
